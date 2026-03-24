3a81b0af89377873f6d29f37d9ad098411d68d6a
Yaniv Etrogi <yaniv.etrogi@gmail.com>
2026-03-24 14:32:15 +0200

    Remove release folders from tracking; add sp_ims_copy_from_sysschedules header comment; ignore publish_output and Release_v* in .gitignore

--- IndexMaintenanceSystem/Scripts/Helpers/sp_ims_copy_from_sysschedules.sql
+++ IndexMaintenanceSystem/Scripts/Helpers/sp_ims_copy_from_sysschedules.sql
@@ -1,3 +1,20 @@
+/*
+ * sp_ims_copy_from_sysschedules
+ *
+ * Copies a SQL Server Agent schedule from msdb.dbo.sysschedules into
+ * dbo.ims_schedules, so it can be used as an IMS maintenance schedule.
+ *
+ * Parameters:
+ *   @sysschedule_id  - The schedule_id of the existing SQL Agent schedule in msdb.
+ *   @schedule_id     - OUTPUT: The new schedule_id inserted into ims_schedules.
+ *
+ * Usage:
+ *   DECLARE @new_id INT;
+ *   EXEC [dbo].[sp_ims_copy_from_sysschedules]
+ *       @sysschedule_id = 42,   -- ID from msdb.dbo.sysschedules
+ *       @schedule_id    = @new_id OUTPUT;
+ *   SELECT @new_id AS NewScheduleId;
+ */
 CREATE PROCEDURE [dbo].[sp_ims_copy_from_sysschedules]
 (
     @sysschedule_id INT,
